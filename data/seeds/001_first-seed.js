7465f343c8cdc60ac4c79ffe211c890165710bc5
Ines Elmufti <ineselmufti@Iness-MacBook-Pro.local>
2021-12-09 23:05:46 +0100

    final

--- data/seeds/001_first-seed.js
+++ data/seeds/001_first-seed.js
@@ -1,14 +1,24 @@
-const profiles = [ {
-  name: 'Imen' },
-  {
-  name: 'Aymen'
-  }, {
-  name: 'Feres' },
-  {
-  name: 'Nadia'
-  }, {
-  name: 'Mohamed' }
-  ];
-  exports.seed = function (knex) { return knex('profiles').del() .then(() => {
-  return knex('profiles').insert(profiles)
-  }) };
+const profiles = [{
+  name: 'Imen',
+  lastname: 'litheb'
+},
+{
+  name: 'Aymen',
+  lastname: 'grala'
+}, {
+  name: 'Feres',
+  lastname: 'el kufti'
+},
+{
+  name: 'Nadia',
+  lastname: 'mufti el'
+}, {
+  name: 'Mohamed',
+  lastname: 'ali benrahma'
+}
+];
+exports.seed = function (knex) {
+  return knex('profiles').del().then(() => {
+    return knex('profiles').insert(profiles)
+  })
+};
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/* allows access to the 'environment variables' (.env file) */ 
+/* allows access to the 'environment variables' (.env file) */
 require('dotenv').config()
-/* import express and cors 2 */ 
-const express = require('express') 
+/* import express and cors 2 */
+const express = require('express')
 const cors = require('cors')
-/* create the server and define the port */ 
+/* create the server and define the port */
 const server = express()
 const port = process.env.PORT || 8000
 /* use cors and express.json (which a body-parser for incoming requests) */
@@ -11,12 +11,13 @@ server.use(cors())
 server.use(express.json())
 /* basic route to access the application in the browser */
 server.get('/', (req, res) => {
-res.send('<h1>This is a test application</h1>')
+    res.send('<h1>This is a test application</h1>')
 })
-/* Listen to the port in the variable and display a message */ 
+/* Listen to the port in the variable and display a message */
 server.listen(port, () => {
-console.log(`\n=== Server listening on port ${port} ===\n`) })
-/* import routes from route.js */ 
+    console.log(`\n=== Server listening on port ${port} ===\n`)
+})
+/* import routes from route.js */
 const testRouter = require('./route')
-/* tell the server to use that router and allows us to use it using ‘/test’ route */ 
+/* tell the server to use that router and allows us to use it using ‘/test’ route */
 server.use('/test', testRouter)
